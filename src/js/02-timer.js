88a50a6a74cfd89b5d2f35d147d62bfd25f9e96f
SheVredina <122560380+SheVredina@users.noreply.github.com>
2023-06-01 10:35:05 +0200

    ментору

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,13 +1,15 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 const inputEl = document.getElementById('datetime-picker');
+
 const refs = {
   daysEl: document.querySelector('[data-days]'),
   hoursEl: document.querySelector('[data-hours]'),
   minutesEl: document.querySelector('[data-minutes]'),
   secondsEl: document.querySelector('[data-seconds]'),
-  startBtn: document.querySelector('[data-start]')
+  startBtn: document.querySelector('[data-start]'),
 };
+refs.startBtn.disabled = true;
 
 const options = {
   enableTime: true,
@@ -15,25 +17,28 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    refs.startBtn.addEventListener('click', () =>{
+    refs.startBtn.disabled = false;
+    refs.startBtn.addEventListener('click', () => {
       countDownTimer.start();
-    })
-
+    });
     const countDownTimer = {
       start() {
         const startTime = new Date(selectedDates);
-    
+
         setInterval(() => {
           const currentTime = Date.now();
           const deltaTime = startTime - currentTime;
-    const time = convertMs(deltaTime);
-    updateClockface(time);
+          const time = convertMs(deltaTime);
+          updateClockface(time);
         }, 1000);
       },
-    }
+    };
   },
-  
 };
+
+// if (deltaTime < 0) {
+//   window.alert ('"Please choose a date in the future"'),
+//   refs.startBtn.disabled = true;
 
 flatpickr(inputEl, options);
 
@@ -45,22 +50,23 @@ function convertMs(deltaTime) {
 
   const days = addLeadingZero(Math.floor(deltaTime / day));
   const hours = addLeadingZero(Math.floor((deltaTime % day) / hour));
-  const minutes = addLeadingZero(Math.floor(((deltaTime % day) % hour) / minute));
-  const seconds = addLeadingZero(Math.floor((((deltaTime % day) % hour) % minute) / second));
+  const minutes = addLeadingZero(
+    Math.floor(((deltaTime % day) % hour) / minute)
+  );
+  const seconds = addLeadingZero(
+    Math.floor((((deltaTime % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
-  };
+}
 
-  function addLeadingZero(value){
-    return String(value).padStart(2, '0');
-  }
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 
-  function updateClockface({ days, hours, minutes, seconds }) {
-    refs.daysEl.textContent = `${days}`;
-    refs.hoursEl.textContent = `${hours}`;
-    refs.minutesEl.textContent = `${minutes}`;
-    refs.secondsEl.textContent = `${seconds}`;
-  }
- 
-
-
+function updateClockface({ days, hours, minutes, seconds }) {
+  refs.daysEl.textContent = `${days}`;
+  refs.hoursEl.textContent = `${hours}`;
+  refs.minutesEl.textContent = `${minutes}`;
+  refs.secondsEl.textContent = `${seconds}`;
+}
